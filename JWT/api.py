4eb6c72d857d97dfdd7fd61e6303d99e081de514
wpr101 <wpr101@hotmail.com>
2017-08-06 10:52:10 -0400

    changes

--- JWT/api.py
+++ JWT/api.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request, make_response
 import jwt
 import datetime
+import logging
 from functools import wraps
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'thisisthesecretkey'
+app.config['SECRET_KEY'] = 'ourkey'
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to the page</h1>'
 
 def token_required(f):
     @wraps(f)
@@ -30,14 +35,16 @@ def unprotected():
 @app.route('/protected')
 @token_required
 def protected():
-    return jsonify({'message' : 'This is only available for people with valid tokens.'})
+    return '<h1>Congratulations, you have unlocked a prize!</h1>'
+    #return jsonify({'message' : 'This is only available for people with valid tokens.'})
 
 #to get the token
 @app.route('/login')
 def login():
     auth = request.authorization
+    app.logger.info("A login is about to be attempted with " + str(auth))
 
-    if auth and auth.password == 'secret2':
+    if auth:# and auth.password == 'secret2':
         token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)}, app.config['SECRET_KEY'])
         return jsonify({'token' : token})
     return (make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'}))
