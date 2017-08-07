8d1830101d149976288c2751e05b496d6db8d03e
wpr101 <wpr101@hotmail.com>
2017-08-06 21:24:47 -0400

    decorator

--- JWT/web2py_api.py
+++ JWT/web2py_api.py
@@ -1,10 +1,27 @@
 import jwt
 import datetime
+from functools import wraps
 
 SECRET_KEY = 'our_secret'
 
 def index(): 
     return 'hello world'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.vars['token'] #http://localhost:5000/route?token=adsfdafdsaf
+
+        if not token:
+            return response.json({'message': 'Token is missing!'})
+
+        try:
+            data = jwt.decode(token, SECRET_KEY)
+        except:
+            return response.json({'message' : 'Token is invalid'})
+            
+        return f(*args, **kwargs)
+    return decorated
 
 def get_token():
     token = jwt.encode({'user' : 'rampage', 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=3600)}, SECRET_KEY)
@@ -20,7 +37,10 @@ def protected():
 
     try:
         data = jwt.decode(token, SECRET_KEY)
-        return ('You have reached the promised land')
+        return ('You have reached the protected resource')
     except:
         return response.json({'message' : 'Token is invalid'})
-    
+
+@token_required
+def protected_v2():
+    return('You have reached the protected resource v2')
