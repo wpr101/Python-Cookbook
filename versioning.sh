41d43605697c7366e7b7ad737a1ca81c2d833b38
wpr101 <wpr101@hotmail.com>
2017-08-11 23:46:55 -0400

    new files

---
+++ versioning.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+
+echo "Make a selection"
+echo "1 Create a branch"
+echo "2 Checkout a branch"
+echo "3 Save and push to your branch"
+
+read choice
+
+if (("$choice" == "1")); then
+  echo "'Create."
+  git checkout -b "new-feature"
+  git push -u origin new-feature
+elif (("$choice" == "2")); then
+  echo "checkout"
+elif (("$choice" == "3")); then
+  echo "save and push"
+fi
